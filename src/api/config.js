f859f6244be590fec87ae1fd76aa64568392f8ee
abhishek <abhishekborpa@gmail.com>
2026-05-20 17:59:40 +0530

    hey

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,5 @@
-export const API_BASE_URL = import.meta.env.VITE_API_URL || "https://manu-back-bpob.onrender.com";
+VITE_API_URL="https://manu-back-bpob.onrender.com/api"
+export const API_BASE_URL = VITE_API_URL || "https://manu-back-bpob.onrender.com";
 
 export const getServerUrl = (path) => {
   if (!path) return "";
